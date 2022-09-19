f7185464975ce000adaec2b5f78f5ebc5bee6727
shawasty <shawasty@gmail.com>
2022-09-18 23:55:58 -0400

    first Java HelloWorld

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, GODBLESS!");
+
+        int myFirstNumber = 15;
+        int mySecondNum = 12;
+        int myThirdNum = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNum + myThirdNum;
+        int myLastOne = myTotal - 1000;
+
+        System.out.println(myLastOne);
+    }
+}
